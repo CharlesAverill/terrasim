a2ed0fc55f3e5af56b8e670ca3310ddca4960d1b
Charles Averill <charlesaverill20@gmail.com>
2023-08-08 15:55:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Friendly OCaml Template
+
+Just run `setup.sh` to set up a ready-for-development OCaml environment
---
+++ setup.sh
@@ -0,0 +1,9 @@
+read -p "Project Name: " PROJECT_NAME
+read -p "Author Name: " AUTHOR_NAME
+read -p "Author Github Username: " AUTHOR_USERNAME
+read -p "Author email (for CI): " AUTHOR_EMAIL
+
+find . -type f -exec sed -i "s/_PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
+touch "test/$PROJECT_NAME.ml"
+
+rm setup.sh
