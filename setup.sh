ecfbbefca2892b9b047083f57ba98694374851b1
Charles Averill <charlesaverill20@gmail.com>
2023-08-08 16:02:51 -0500

    Update docs

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ read -p "Author email (for CI): " AUTHOR_EMAIL
 
 find . -type f -exec sed -i "s/_PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
 touch "test/$PROJECT_NAME.ml"
+echo "dir: ./docs" > "$PROJECT_NAME.odocl"
+echo -e "{0 Index}\n\nHello World!" > "docs/$PROJECT_NAME.mld"
 
 rm setup.sh
+rm -rf .git
