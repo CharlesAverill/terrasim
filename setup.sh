cae1ded01bf5028b3d53a1b1eafa5984c3e04f6a
Charles Averill <charlesaverill20@gmail.com>
2024-09-01 00:22:12 -0500

    More updates

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ read -p "Author email (for CI): " AUTHOR_EMAIL
 
 find . -type f -exec sed -i "s/_PROJECT_SYNOPSIS_/$PROJECT_SYNOPSIS/g; s/_PROJECT_DESCRIPTION_/$PROJECT_DESCRIPTION/g; s/PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
 echo "dir: ./docs" > "$PROJECT_NAME.odocl"
+rm docs/PROJECT_NAME_.mld
 echo -e "{0 Index}\n\nHello World!" > "docs/$PROJECT_NAME.mld"
 
 rm setup.sh
