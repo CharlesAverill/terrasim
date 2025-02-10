97356a6e1d2eaf8cbc0a14d35c03a9660b9dfdbe
Charles Averill <charlesaverill20@gmail.com>
2025-02-10 16:58:27 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ read -p "Author Name: " AUTHOR_NAME
 read -p "Author Github Username: " AUTHOR_USERNAME
 read -p "Author email (for CI): " AUTHOR_EMAIL
 
+mv .git "../.git.$PROJECT_NAME"
+
 find . -type f -exec sed -i "s/_PROJECT_SYNOPSIS_/$PROJECT_SYNOPSIS/g; s/_PROJECT_DESCRIPTION_/$PROJECT_DESCRIPTION/g; s/PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
 echo "dir: ./docs" > "$PROJECT_NAME.odocl"
 rm docs/PROJECT_NAME_.mld
@@ -17,3 +19,5 @@ echo "$PROJECT_DESCRIPTION" >> README.md
 
 rm setup.sh
 rm PROJECT_NAME_.opam
+
+mv "../.git.$PROJECT_NAME" .git
