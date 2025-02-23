2c977dd0d7fefe8a88b70682ff6aecea57bb485a
Charles Averill <charlesaverill20@gmail.com>
2025-02-23 13:54:52 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 read -p "Project Name: " PROJECT_NAME
 read -p "Project Description: " PROJECT_DESCRIPTION
 read -p "Project Synopsis: " PROJECT_SYNOPSIS
+read -p "Project License (identifier from https://spdx.org/licenses/): " PROJECT_LICENSE
 read -p "Author Name: " AUTHOR_NAME
 read -p "Author Github Username: " AUTHOR_USERNAME
 read -p "Author email (for CI): " AUTHOR_EMAIL
 
 mv .git "../.git.$PROJECT_NAME"
 
-find . -type f -exec sed -i "s/_PROJECT_SYNOPSIS_/$PROJECT_SYNOPSIS/g; s/_PROJECT_DESCRIPTION_/$PROJECT_DESCRIPTION/g; s/PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
+find . -type f -exec sed -i "s/_PROJECT_LICENSE_/$PROJECT_LICENSE/g; s/_PROJECT_SYNOPSIS_/$PROJECT_SYNOPSIS/g; s/_PROJECT_DESCRIPTION_/$PROJECT_DESCRIPTION/g; s/PROJECT_NAME_/$PROJECT_NAME/g; s/_AUTHOR_NAME_/$AUTHOR_NAME/g; s/_AUTHOR_USERNAME_/$AUTHOR_USERNAME/g; s/_AUTHOR_EMAIL_/$AUTHOR_EMAIL/g" {} +
 echo "dir: ./docs" > "$PROJECT_NAME.odocl"
 rm docs/PROJECT_NAME_.mld
 echo -e "{0 Index}\n\nHello World!" > "docs/$PROJECT_NAME.mld"
